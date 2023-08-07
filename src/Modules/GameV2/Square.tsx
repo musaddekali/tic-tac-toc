77d5ecf139bf54b2b9dbc918afbe5930a2890af1
MusaddekAli <musaddekali.dev@gmail.com>
2023-08-07 10:42:55 +0600

    border color changed

--- src/Modules/GameV2/Square.tsx
+++ src/Modules/GameV2/Square.tsx
@@ -10,7 +10,7 @@ type Prop = {
 const Square = ({position, value, onPlay, winner, winnerPositions }: Prop) => {
   const winnerBox = winner && winnerPositions?.includes(position);
   return (
-    <button onClick={onPlay} className={`square w-20 h-20 border font-bold text-2xl ${winnerBox ? 'bg-blue-500 text-white' : ''}`}>
+    <button onClick={onPlay} className={`square w-20 h-20 border border-blue-500 font-bold text-2xl ${winnerBox ? 'bg-blue-500 text-white' : ''}`}>
       {value}
     </button>
   );
