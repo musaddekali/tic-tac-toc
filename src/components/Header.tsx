6e62559cecd97985635db4a52bfe675457fc7bb9
MusaddekAli <musaddekali.dev@gmail.com>
2023-08-07 10:10:16 +0600

    ok

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,19 +7,13 @@ function Header() {
         <div className="container">
           <ul className="flex items-center justify-center gap-8">
             <li>
-              <Link href="/">
-                <span className="text-slate-800 hover:underline hover:text-blue-500">Home</span>
-              </Link>
+              <Link href="/">Home</Link>
             </li>
             <li>
-              <Link href="/game_v1">
-                <span className="text-slate-800 hover:underline hover:text-blue-500">GameV1</span>
-              </Link>
+              <Link href="/game_v1">GameV1</Link>
             </li>
             <li>
-              <Link href="/">
-                <span className="text-slate-800 hover:underline hover:text-blue-500">GameV2</span>
-              </Link>
+              <Link href="/">GameV2</Link>
             </li>
           </ul>
         </div>
